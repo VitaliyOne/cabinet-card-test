5372268de3bcd09bbccd9cd5959035a422443e6f
VitaliyOne <vitaliy.one1@yandex.ru>
2024-03-27 13:11:05 +0300

    Add tooltips on first picture

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
+const circles = document.querySelectorAll('.productImgRound');
+const tooltips = document.querySelectorAll('.productRoundTooltip');
+if (circles.length > 0 && tooltips.length > 0) {
+  circles.forEach((circle, index) => {
+    let tooltipVisible = false;
+    circle.addEventListener('click', () => {
+      if (tooltips[index]) {
+        if (tooltipVisible) {
+          tooltips[index].style.opacity = '0';
+          tooltipVisible = false;
+        } else {
+          tooltips.forEach(tooltip => {
+            tooltip.style.opacity = '0';
+          });
+          tooltips[index].style.opacity = '1';
+          tooltipVisible = true;
+        }
+      }
+    });
+  });
+}
+
 const productDescriptionShowOnClick = () => {
   const element = document.querySelector('.productDescriptionShow');
   const description = document.querySelector('.productDescriptionActive');
