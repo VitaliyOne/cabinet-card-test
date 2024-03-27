bcf4827a376a316956e47dc15b1a1c41c0634d85
VitaliyOne <vitaliy.one1@yandex.ru>
2024-03-27 11:49:47 +0300

    Update styles, add unfolding product description

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-
+const productDescriptionShowOnClick = () => {
+  const element = document.querySelector('.productDescriptionShow');
+  const description = document.querySelector('.productDescriptionActive');
+  if (element && description) {
+    element.addEventListener('click', () => {
+      description.classList.toggle('productDescriptionDisable');
+      description.classList.toggle('productDescriptionActiveDisable');
+      element.textContent === 'Читать полностью' ? element.textContent = 'Свернуть' : element.textContent = 'Читать полностью';
+    });
+  }
+}
+productDescriptionShowOnClick();
