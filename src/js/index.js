84110d6e26bc59c74433eb975a9ef4237247d016
VitaliyOne <vitaliy.one1@yandex.ru>
2024-03-28 17:29:12 +0300

    Add product form functionality, code review

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,25 @@
-const circles = document.querySelectorAll('.productImgRound');
-const tooltips = document.querySelectorAll('.productRoundTooltip');
-if (circles.length > 0 && tooltips.length > 0) {
-  circles.forEach((circle, index) => {
-    let tooltipVisible = false;
-    circle.addEventListener('click', () => {
-      if (tooltips[index]) {
-        if (tooltipVisible) {
-          tooltips[index].style.opacity = '0';
-          tooltipVisible = false;
-        } else {
-          tooltips.forEach(tooltip => {
-            tooltip.style.opacity = '0';
-          });
-          tooltips[index].style.opacity = '1';
-          tooltipVisible = true;
+const setupProductImageTooltips = () => {
+  const circles = document.querySelectorAll('.productImgRound');
+  const tooltips = document.querySelectorAll('.productRoundTooltip');
+  if (circles.length > 0 && tooltips.length > 0) {
+    circles.forEach((circle, index) => {
+      let tooltipVisible = false;
+      circle.addEventListener('click', () => {
+        if (tooltips[index]) {
+          if (tooltipVisible) {
+            tooltips[index].style.opacity = '0';
+            tooltipVisible = false;
+          } else {
+            tooltips.forEach(tooltip => {
+              tooltip.style.opacity = '0';
+            });
+            tooltips[index].style.opacity = '1';
+            tooltipVisible = true;
+          }
         }
-      }
+      });
     });
-  });
+  }
 }
 
 const productDescriptionShowOnClick = () => {
@@ -31,15 +33,68 @@ const productDescriptionShowOnClick = () => {
     });
   }
 }
-productDescriptionShowOnClick();
+
+const updateTotalPriceVisibility = (totalPrice) => {
+  const productNoComponents = document.querySelector('.productNoComponents');
+  const productSomeComponents = document.querySelector('.productSomeComponents');
+
+  if (productNoComponents && productSomeComponents) {
+    if (totalPrice !== 0) {
+      productNoComponents.style.display = 'none';
+      productSomeComponents.style.display = 'flex';
+    } else {
+      productNoComponents.style.display = 'flex';
+      productSomeComponents.style.display = 'none';
+    }
+  }
+}
+
+const updateTotalPrice = () => {
+  const totalPriceElement = document.getElementById('totalPrice');
+  let totalPrice = 0;
+  document.querySelectorAll('.productItemCounter').forEach(item => {
+    const price = item.querySelector('.productItemInput').getAttribute('price')
+    const quantity = parseInt(item.querySelector('.productItemInput').value);
+    if (price && quantity) {
+      totalPrice += price * quantity;
+    }
+  });
+  if (totalPriceElement) {
+    totalPriceElement.textContent = totalPrice.toLocaleString('ru-RU') + ' руб';
+    updateTotalPriceVisibility(totalPrice)
+  }
+};
+
+const addCounterButtonsEventListeners = () => {
+  const counterPlusButtons = document.querySelectorAll('.productItemCounterArrowTop');
+  const counterMinusButtons = document.querySelectorAll('.productItemCounterArrowBottom')
+  if (counterPlusButtons.length > 0) {
+    counterPlusButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const input = button.parentNode.querySelector('.productItemInput');
+        if (input) {
+          input.value = parseInt(input.value) + 1;
+          updateTotalPrice();
+        }
+      });
+    })
+  }
+  if (counterMinusButtons.length > 0) {
+    counterMinusButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const input = button.parentNode.querySelector('.productItemInput');
+        if (input && parseInt(input.value) > 0) {
+          input.value = parseInt(input.value) - 1;
+          updateTotalPrice();
+        }
+      });
+    })
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  const modalContent = document.getElementById('modal-content');
-  if (modalContent) {
-    modalContent.addEventListener('click', (event) => {
-      if (event.target === modalContent) {
-        modalContent.style.display = 'none';
-      }
-    });
-  }
+  setupProductImageTooltips();
+  productDescriptionShowOnClick();
+  updateTotalPrice();
+  addCounterButtonsEventListeners();
 });
